7c0a8740f154309200a9520a9e1b6beef20d3b74
Satriokml <satriokamil2@gmail.com>
2024-03-18 21:17:51 +0700

    add weather api

--- controllers/Irrigation.js
+++ controllers/Irrigation.js
@@ -1,4 +1,9 @@
 import IrrigationDataModel from "../models/IrrigationDataModel.js";
+import axios from "axios";
+
+
+const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
+const WEATHER_API_URL = process.env.WEATHER_API_URL;
 
 
 export const getData = async(req, res) =>{
@@ -9,6 +14,28 @@ export const getData = async(req, res) =>{
     } catch (error) {
         res.status(500).json({msg: error.message});
     }
+}
+
+export const getWeather = async(req, res) =>{
+    try {
+        const latitude = process.env.latitude;
+        const longitude = process.env.longitude;
+
+        const response = await axios.get(WEATHER_API_URL, {
+          params: {
+            key: WEATHER_API_KEY,
+            q: `${latitude},${longitude}`,
+            aqi: 'no'
+          }
+        });
+
+        const weatherData = response.data;
+        res.send(weatherData.current.condition.text);
+      } catch (error) {
+
+        console.error('Error fetching weather data:', error);
+        res.status(500).json({ error: 'Error fetching weather data' });
+      }
 }
 
 export const createData = async(req, res) =>{
